8d52643d40bfa2adabd592277cc0f61145b26502
vishu vatsay <vishusharma9520@gmail.com>
2026-03-22 22:26:38 +0530

    register user and bug fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
-router.route("/resgister").post(registerUser)
+router.route("/resgister").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser)
 
 
 
