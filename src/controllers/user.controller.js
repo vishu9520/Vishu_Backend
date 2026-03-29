448c7135d8e478ca1692ef22c6584fead5693024
vishu vatsay <vishusharma9520@gmail.com>
2026-03-29 09:28:05 +0530

    update passowrd

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,9 +233,56 @@ const refreshAccessToken= asyncHandler(async(req,res)=>{
     throw new ApiError(401,error?.message||"Invalid refresh token")
  }
 })
+
+const changeCurrentPassword = asyncHandler(async (req,res)=>{
+    const {oldPassword, newPassword ,confPassword} = req.body;
+   
+
+ const user = await User.findById(req.user?._id)
+const isPasswordCorrect =await user.isPasswordCorrect(oldPassword)
+if(!isPasswordCorrect){
+    throw new ApiError(400,"Invalid old password")
+}
+
+user.password = newPassword
+ await user.save({validateBeforeSave:false})// this line may cause server error in this route
+
+ return res
+ .status(200)
+ .json(new ApiResponse(200,{},"Password changed successfully"))
+})
+const getCurrentUser =asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetch successfully")
+})
+
+const updateAccountDetails =asyncHandler(async(req,res)=>{
+    const {fullName,email} = req.body
+    if(!fullName||!email){
+        throw new ApiError(400,"All fields are required")
+    }
+  const user=  User.findByIdAndUpdate(
+        req.user?._id,             // check this line if error occur
+        {
+            $set:{
+                fullName,
+                email:email
+            }
+        },
+        {new:true}
+    ).select("-password")
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+const updateUserAvatar = asyncHandler(asserts(req,res)=>{})
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    getCurrentUser,
+    changeCurrentPassword,
+    updateAccountDetails
 }
